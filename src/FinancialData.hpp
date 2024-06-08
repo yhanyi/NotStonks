db3b6f8f8d5bda2a3334e9fb2acb3b9bdea49c94
yhanyi <yeohhanyi0916@gmail.com>
2024-06-08 14:08:54 +0800

    Added FinancialData

---
+++ src/FinancialData.hpp
@@ -0,0 +1,29 @@
+#ifndef FINANCIALDATA_HPP
+#define FINANCIALDATA_HPP
+
+#include <string>
+#include <vector>
+
+struct DataRow {
+    std::string date;
+    double open;
+    double high;
+    double low;
+    double close;
+    double volume;
+    double adj_close;
+};
+
+class FinancialData {
+   public:
+    FinancialData(const std::string& filename);
+
+    const std::vector<DataRow>& get_data() const;
+    std::vector<double> get_closing_prices() const;
+
+   private:
+    std::vector<DataRow> data;
+    void read_csv(const std::string& filename);
+};
+
+#endif  // FINANCIALDATA_HPP
