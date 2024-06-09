fab978a545a8f40dc3d83cf573cd977ef170aff3
yhanyi <yeohhanyi0916@gmail.com>
2024-06-09 14:46:46 +0800

    Added EventStudy

---
+++ src/EventStudy.cpp
@@ -0,0 +1,20 @@
+#include "EventStudy.hpp"
+
+#include <numeric>
+
+std::vector<double> EventStudy::calculate_abnormal_returns(const std::vector<double>& returns, const std::vector<double>& market_returns) {
+    if (returns.size() != market_returns.size()) {
+        throw std::invalid_argument("Returns series must have the same length");
+    }
+
+    std::vector<double> abnormal_returns(returns.size());
+    for (size_t i = 0; i < returns.size(); ++i) {
+        abnormal_returns[i] = returns[i] - market_returns[i];
+    }
+
+    return abnormal_returns;
+}
+
+double EventStudy::cumulative_abnormal_return(const std::vector<double>& abnormal_returns) {
+    return std::accumulate(abnormal_returns.begin(), abnormal_returns.end(), 0.0);
+}
