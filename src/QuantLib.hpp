1537b54ef31526062800334bdc95772147a8fd43
yhanyi <yeohhanyi0916@gmail.com>
2024-06-09 12:49:02 +0800

    Updated QuantLib header

--- src/QuantLib.hpp
+++ src/QuantLib.hpp
@@ -2,8 +2,11 @@
 #define QUANTLIB_HPP
 
 #include "Analysis.hpp"
+#include "Backtesting.hpp"
 #include "FinancialData.hpp"
 #include "MonteCarlo.hpp"
 #include "PortfolioOptimization.hpp"
+#include "RiskMetrics.hpp"
+#include "TechnicalIndicators.hpp"
 
 #endif  // QUANTLIB_HPP
