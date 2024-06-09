c4e3944368f5c1bbd9f46d04aeab5c5ad197660e
yhanyi <yeohhanyi0916@gmail.com>
2024-06-09 15:49:37 +0800

    Updated QuantLib header

--- src/QuantLib.hpp
+++ src/QuantLib.hpp
@@ -1,16 +1,25 @@
 #ifndef QUANTLIB_HPP
 #define QUANTLIB_HPP
 
+#include "ARIMA.hpp"
 #include "Analysis.hpp"
 #include "Backtesting.hpp"
+#include "BinomialTree.hpp"
 #include "BlackScholes.hpp"
+#include "Cointegration.hpp"
 #include "EventStudy.hpp"
 #include "FinancialData.hpp"
+#include "GARCH.hpp"
 #include "Greeks.hpp"
+#include "LinearRegression.hpp"
+#include "MarketImpactModels.hpp"
 #include "MonteCarlo.hpp"
+#include "OrderBook.hpp"
 #include "PairsTrading.hpp"
 #include "PortfolioOptimization.hpp"
+#include "RiskManagement.hpp"
 #include "RiskMetrics.hpp"
 #include "TechnicalIndicators.hpp"
+#include "TradeVolumeAnalysis.hpp"
 
 #endif  // QUANTLIB_HPP
