32b98fa06636a70d9d4cd3a905a6d61dec7bdc6d
yhanyi <yeohhanyi0916@gmail.com>
2024-06-12 19:06:36 +0800

    Updated Benchmark

--- src/Benchmark.cpp
+++ src/Benchmark.cpp
@@ -2,19 +2,22 @@
 
 #include <iostream>
 
+#include "MeanReversion.hpp"
+#include "QuantLib.hpp"
+
 Benchmark::Benchmark(const std::string& csv_file) : fd(csv_file) {}
 
-std::map<std::string, double> Benchmark::evaluate_strategy(const std::vector<int>& signals) {
-    std::vector<double> closing_prices = fd.get_closing_prices();
-    double final_portfolio_value = EventDrivenBacktesting::simulate(closing_prices, signals, 10000, 1);
-    std::vector<double> returns;
-    for (size_t i = 1; i < closing_prices.size(); ++i) {
-        returns.push_back((closing_prices[i] - closing_prices[i - 1]) / closing_prices[i - 1]);
-    }
-    double sharpe = RiskAdjustedPerformance::sharpe_ratio(returns, 0.001);
+// std::map<std::string, double> Benchmark::evaluate_strategy(const std::vector<int>& signals) {
+//     std::vector<double> closing_prices = fd.get_closing_prices();
+//     double final_portfolio_value = EventDrivenBacktesting::simulate(closing_prices, signals, 10000, 1);
+//     std::vector<double> returns;
+//     for (size_t i = 1; i < closing_prices.size(); ++i) {
+//         returns.push_back((closing_prices[i] - closing_prices[i - 1]) / closing_prices[i - 1]);
+//     }
+//     double sharpe = RiskAdjustedPerformance::sharpe_ratio(returns, 0.001);
 
-    return {{"Final Portfolio Value", final_portfolio_value}, {"Sharpe Ratio", sharpe}};
-}
+//     return {{"Final Portfolio Value", final_portfolio_value}, {"Sharpe Ratio", sharpe}};
+// }
 
 void Benchmark::report_results(const std::map<std::string, double>& results, const std::string& strategy_name) {
     std::cout << "Results for " << strategy_name << " strategy:" << std::endl;
@@ -29,9 +32,14 @@ void Benchmark::run() {
     std::vector<double> closing_prices = fd.get_closing_prices();
 
     // Mean Reversion Strategy
-    std::vector<int> mean_reversion_signals = MeanReversion::generate_signals(closing_prices, 1.0);
-    auto mean_reversion_results = evaluate_strategy(mean_reversion_signals);
-    report_results(mean_reversion_results, "Mean Reversion");
+    // int window_size = 20;
+    // double entry_threshold = 2.0;
+    // double exit_threshold = 1.0;
+
+    // auto z_scores = MeanReversion::calculate_z_score(closing_prices, window_size);
+    // auto signals = MeanReversion::generate_signals(z_scores, entry_threshold, exit_threshold);
+    // double final_portfolio_value = MeanReversion::backtest_strategy(signals, closing_prices, 10000);
+    // std::cout << "Final Portfolio Value: " << final_portfolio_value << std::endl;
 
     // Add more strategies here
     std::cout << "--------------------------- BENCHMARK END ---------------------------" << std::endl;
