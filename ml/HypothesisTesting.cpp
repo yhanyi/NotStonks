887526412a51c43bb03a1aac777d5f5341616221
yhanyi <yeohhanyi0916@gmail.com>
2024-06-13 23:12:59 +0800

    Updated HypothesisTesting

--- ml/HypothesisTesting.cpp
+++ ml/HypothesisTesting.cpp
@@ -2,16 +2,11 @@
 
 #include <vector>
 
-namespace MLCPP {
-class HypothesisTesting {
-   public:
-    std::pair<double, int> chiSquaredStatistic(std::vector<double> observed, std::vector<double> expected) {
-        double df = observed.size() - 1;
-        double total = 0;
-        for (int i = 0; i < observed.size(); i++) {
-            total += (observed[i] - expected[i]) * (observed[i] - expected[i]) / expected[i];
-        }
-        return std::make_pair(total, df);
+std::pair<double, int> chiSquaredStatistic(std::vector<double> observed, std::vector<double> expected) {
+    double df = observed.size() - 1;
+    double total = 0;
+    for (int i = 0; i < observed.size(); i++) {
+        total += (observed[i] - expected[i]) * (observed[i] - expected[i]) / expected[i];
     }
-};
-}  // namespace MLCPP
+    return std::make_pair(total, df);
+}
--- ml/HypothesisTesting.hpp
+++ ml/HypothesisTesting.hpp
@@ -5,11 +5,9 @@
 
 #include <vector>
 
-namespace MLCPP {
 class HypothesisTesting {
    public:
-    std::pair<double, int> chiSquaredStatistic(std::vector<double> observed, std::vector<double> expected);
+    static std::pair<double, int> chiSquaredStatistic(std::vector<double> observed, std::vector<double> expected);
 };
-}  // namespace MLCPP
 
 #endif  // HYPOTHESISTESTING_HPP
